661ce8c6409770a0b47230cc32a73be75d21244b
Karan Khatri <karankhatri535@gmail.com>
2023-11-27 14:10:17 +0530

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow us</h5>
+            <div>
+              <a href="#"> Youtube </a>
+              <a href="#"> Instagram </a>
+              <a href="#"> Github </a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
